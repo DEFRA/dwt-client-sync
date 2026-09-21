4cd2d0497d59595bfb52c6e1c5943beb8152c341
Chris Renwick <chris.p.renwick@gmail.com>
2026-09-21 15:47:56 +0100

    DWTA-335: cognito response schema changed so amending to match

--- src/services/client-sync.js
+++ src/services/client-sync.js
@@ -31,17 +31,17 @@ async function sync() {
 
         const credentials = await allCognitoCredentials(service)
 
-        if (!credentials || !credentials.body.client_details) {
+        if (!credentials || !credentials.client_details) {
           logger.error('No credentials found in Cognito response')
           result.services.push({ serviceName: service, credentialsSynced: 0 })
           continue
         }
 
         logger.info(
-          `Fetched ${credentials.body.client_details.length} Cognito credentials`
+          `Fetched ${credentials.client_details.length} Cognito credentials`
         )
 
-        const softwareProviders = credentials.body.client_details.map(
+        const softwareProviders = credentials.client_details.map(
           ({ client_name: clientName, client_id: clientId }) => [
             { text: clientName },
             { text: clientId }
